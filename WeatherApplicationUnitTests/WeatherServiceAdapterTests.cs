305c93cb43551b8bcdf3a698230ac21243310f43
mdrab <mateusz.drab@dfg.com>
2017-12-18 18:37:20 +0100

    fg

--- WeatherApplicationUnitTests/WeatherServiceAdapterTests.cs
+++ WeatherApplicationUnitTests/WeatherServiceAdapterTests.cs
@@ -1,14 +1,43 @@
 ﻿using Xunit;
 using NSubstitute;
+using WeatherApplicationAPI.WeatherService.Abstraction;
+using WeatherApplicationAPI.WeatherService;
+using AutoMapper;
 
 namespace WeatherApplicationUnitTests
 {
     public class WeatherServiceAdapterTests
     {
         [Fact]
-        public void Test()
+        public void WeatherSerivceAdapterShouldCallWeatherService()
         {
+            var weatherService = Substitute.For<WeatherServiceRestful.WeatherServiceRestful>();
+            var mapper = Substitute.For<IMapper>();
 
+            IWeatherService serviceAdapter = new WeatherServiceAdapter(weatherService, mapper);
+
+            string city = "city";
+            string country = "country";
+
+            serviceAdapter.GetCurrentWeatherForecast(city, country);
+
+            weatherService.Received(1).CallWeatherService(Arg.Is<string>(city), Arg.Is<string>(country));
+        }
+
+        [Fact]
+        public void WeatherSerivceAdapterShouldReturnWeather()
+        {
+            var weatherService = Substitute.For<WeatherServiceRestful.WeatherServiceRestful>();
+            var mapper = Substitute.For<IMapper>();
+
+            IWeatherService serviceAdapter = new WeatherServiceAdapter(weatherService, mapper);
+
+            //string city = "city";
+            //string country = "country";
+
+            //serviceAdapter.GetCurrentWeatherForecast(city, country);
+
+            //weatherService.Received(1).CallWeatherService(Arg.Is<string>(city), Arg.Is<string>(country));
         }
     }
 }
