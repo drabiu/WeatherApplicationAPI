b4a9a9cca7bc517114f9de38fae49408e880912f
mdrab <mateusz.drab@dfg.com>
2017-12-18 21:29:48 +0100

    fdr

--- WeatherApplicationUnitTests/WeatherServiceAdapterTests.cs
+++ WeatherApplicationUnitTests/WeatherServiceAdapterTests.cs
@@ -3,6 +3,8 @@ using NSubstitute;
 using WeatherApplicationAPI.WeatherService.Abstraction;
 using WeatherApplicationAPI.WeatherService;
 using AutoMapper;
+using RestSharp;
+using WeatherApplicationAPI.Models;
 
 namespace WeatherApplicationUnitTests
 {
@@ -11,7 +13,9 @@ namespace WeatherApplicationUnitTests
         [Fact]
         public void WeatherSerivceAdapterShouldCallWeatherService()
         {
-            var weatherService = Substitute.For<WeatherServiceRestful.WeatherServiceRestful>();
+            var config = Substitute.For<WeatherServiceConfiguration>();
+            var restClient = Substitute.For<RestClient>();
+            var weatherService = Substitute.For<WeatherServiceRestful.WeatherServiceRestful>(config, restClient);
             var mapper = Substitute.For<IMapper>();
 
             IWeatherService serviceAdapter = new WeatherServiceAdapter(weatherService, mapper);
@@ -27,17 +31,26 @@ namespace WeatherApplicationUnitTests
         [Fact]
         public void WeatherSerivceAdapterShouldReturnWeather()
         {
-            var weatherService = Substitute.For<WeatherServiceRestful.WeatherServiceRestful>();
+            var config = Substitute.For<WeatherServiceConfiguration>();
+            var restClient = Substitute.For<RestClient>();
+            var weatherService = Substitute.For<WeatherServiceRestful.WeatherServiceRestful>(config, restClient);
+
             var mapper = Substitute.For<IMapper>();
+            var forecast = new WeatherForecast();
+            forecast.temperature = new Temperature();
+            forecast.temperature.value = 16;
+            forecast.humidity = 88;
+            mapper.Map<WeatherForecast>(Arg.Any<WeatherForecast>()).Returns(forecast);
 
             IWeatherService serviceAdapter = new WeatherServiceAdapter(weatherService, mapper);
 
-            //string city = "city";
-            //string country = "country";
+            string city = "city";
+            string country = "country";
 
-            //serviceAdapter.GetCurrentWeatherForecast(city, country);
+            var result = serviceAdapter.GetCurrentWeatherForecast(city, country);
 
-            //weatherService.Received(1).CallWeatherService(Arg.Is<string>(city), Arg.Is<string>(country));
+            Assert.Equal(88, result.humidity);
+            Assert.Equal(16, result.temperature.value);
         }
     }
 }
